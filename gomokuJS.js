5c37b894883f3f099894e24ba08b1e7ee5c37284
Wang Guibao <wang_guibao@163.com>
2017-10-28 17:49:44 +0800

    Winning judge, aware of long-chain

--- gomokuJS.js
+++ gomokuJS.js
@@ -7,11 +7,12 @@
  */
 function Board() {
     this.DIMENSION = 15;
+    this.UNOCCUPIED = -1;
     this.matrix = new Array(15);
     for (var i = 0; i < 15; i += 1) {
         this.matrix[i] = new Array(15);
         for (var j = 0; j < 15; j++) {
-            this.matrix[i][j] = -1;
+            this.matrix[i][j] = UNOCCUPIED;
         }
     }
 }
@@ -30,50 +31,103 @@ Board.prototype = {
         this.matrix[x][y] = op;
 
         var chain = 0;
+        var maxChain = 0;
         var i = 0;
         var j = 0;
         var win = false;
         // chain of left-right
-        for (i = Math.max(x - 4, 0); i <= Math.min(x, this.DIMENSION - 5); ++i) {
-            chain = 0;
-            win = false;
-            for (j = i; j < i + 5; ++j) {
-                if (this.matrix[j][y] != op) {
-                    break;
-                }
+        for (i = Math.max(x - 5, 0); i <= Math.min(x + 5, this.DIMENSION - 1); ++i) {
+            if (this.matrix[i][y] != op) {
+                    maxChain = Math.max(chain, maxChain);
+                    chain = 0;
+            }
+            else {
                 chain++;
-            }
-            if (chain == 5) {
-                win = true;
-                window.alert('You win');
-                return 0;
-            }
-        }
-        // chain of up-down
-        for (i = Math.max(y - 4, 0); i <= Math.min(y, this.DIMENSION - 5); ++i) {
-            chain = 0;
-            win = false;
-            for (j = i; j < i + 5; ++j) {
-                if (this.matrix[x][j] != op) {
-                    break;
-                }
-                chain++;
-            }
-            if (chain == 5) {
-                win = true;
-                window.alert('You win');
-                return 0;
             }
         }
 
+        if (maxChain == 0) {
+            maxChain = chain;
+        }
+        if (maxChain == 5) {
+            win = true;
+            window.alert('You win');
+            return 0;
+        }
+
+        // chain of up-down
+        chain = 0;
+        win = false;
+        maxChain = 0;
+        for (i = Math.max(y - 5, 0); i <= Math.min(y + 5, this.DIMENSION - 1); ++i) {
+            if (this.matrix[x][i] != op) {
+                maxChain = Math.max(chain, maxChain);
+                chain = 0;
+            }
+            else {
+                chain++;
+            }
+        }
+
+        if (maxChain == 0) {
+            maxChain = chain;
+        }
+        if (maxChain == 5) {
+            win = true;
+            window.alert('You win');
+            return 0;
+        }
+
         // chain of bottomleft-upright
-        // for (i = Math.max(x - 4, 0); i <= Math.min(x, this.DIMENSION - 5); ++i) {
-        //    chain = 0;
-        //    win = false;
-        //    for (j = i;j < i + 5; ++j) {
-        //        if (matrix[][])
-        //    }
-        // }
+        chain = 0;
+        maxChain = 0;
+        win = false;
+        for (i = Math.max(x - 5, 0); i <= Math.min(x + 5, this.DIMENSION - 1); ++i) {
+            j = i - (x - y);
+            if (j < 0) {
+                continue;
+            }
+            if (this.matrix[i][j] != op) {
+                maxChain = Math.max(chain, maxChain);
+                chain = 0;
+            }
+            else {
+                chain++;
+            }
+        }
+
+        if (maxChain == 0) {
+            maxChain = chain;
+        }
+        if (maxChain == 5) {
+            win = true;
+            window.alert('You win');
+            return 0;
+        }
+
+        // chain of bottomright-upleft
+        chain = 0;
+        maxChain = 0;
+        win = false;
+        for (i = Math.max(x - 5, 0); i <= Math.min(x + 5, this.DIMENSION - 1); ++i) {
+            j = (x + y) - i;
+            if (this.matrix[i][j] != op) {
+                maxChain = Math.max(chain, maxChain);
+                chain = 0;
+            }
+            else {
+                chain++;
+            }
+        }
+
+        if (maxChain == 0) {
+            maxChain = chain;
+        }
+        if (maxChain == 5) {
+            win = true;
+            window.alert('You win');
+            return 0;
+        }
     }
 };
 
